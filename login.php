cc7edbef2234f02d912d7785388b15bf44e77c8d
Samuel Lieber <36018073+samueljlieber@users.noreply.github.com>
2019-12-03 20:52:38 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php include('heading.php');?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 header-login">
+            <div class="row h-100 m-0 p-0">                
+                <h1 class="my-auto">Login</h1>
+            </div>
+        </div>
+        <div class="col-sm-8 col-md-6 mt-5 mx-auto">
+            <form action="">
+                <fieldset>
+                    <div class="row">
+                        <legend class="sr-only">Login to account</legend>
+                        <div class="col-md-6 px-2">
+                            <label for="" class="input-label">Username or Email</label>
+                            <input type="text" id="" class="input-field">
+                        </div>
+
+                        <div class="col-md-6 px-2">
+                            <label for="" class="input-label">Password</label>
+                            <input type="password" id="" class="input-field">
+                        </div>  
+                        <input type="submit" id="" class="secondary-btn" value="Login">
+                    </div>
+                </fieldset>                
+            </form>
+        </div>
+    </div>
+</div>
+<?php include('footer.php');?>
