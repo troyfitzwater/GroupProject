5824f91981e954f6f310262c051442761852eecd
Samuel Lieber <36018073+samueljlieber@users.noreply.github.com>
2019-12-04 15:38:50 -0500

    Create continueLearning

---
+++ continueLearning.php
@@ -0,0 +1,77 @@
+
+<?php include('heading.php');?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 header-categories">
+            <div class="col-12 text-left">
+                <a href="navigation.php" class="navigation-btn" id="navigation-btn">
+                    <img src="images/hamburg.png" alt="Navigation Menu">
+                </a>
+            </div>     
+            <div class="row h-100 m-0 p-0">                         
+                <h1 class="my-auto">Continue Learning</h1>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-6 mt-5 mx-auto"> 
+            <div class="row">
+                <h2>Available Categories</h2>         
+                <div class="category-wrapper text-center col-12">                
+                    <!-- Loop through avilable categories -->
+                    <a href="#" alt="CompTIA Category" class="category-option">
+                        <div class="row">
+                            <div class="col-6 my-auto p-0 m-0">
+                                <img src="images/compTIA-category.png" alt="compTIA Category" class="img-fluid">
+                            </div>
+                            <div class="col-6 my-auto p-0 m-0">
+                                <p>Decks: 5</p>
+                                <p>Cards: 60</p>
+                            </div>
+                        </div>                        
+                    </a>
+                    <hr>
+                     <!-- END Loop -->
+
+                     <!-- EXAMPLES -->
+                     <a href="#" alt="CompTIA Category" class="category-option">
+                        <div class="row">
+                            <div class="col-6 my-auto p-0 m-0">
+                                <img src="images/networking1-category.png" alt="compTIA Category" class="img-fluid">
+                            </div>
+                            <div class="col-6 my-auto p-0 m-0">
+                                <p>Decks: 5</p>
+                                <p>Cards: 60</p>
+                            </div>
+                        </div>                        
+                    </a>
+                    <hr>
+                    <a href="#" alt="CompTIA Category" class="category-option">
+                        <div class="row">
+                            <div class="col-6 my-auto p-0 m-0">
+                                <img src="images/ceh-category.png" alt="compTIA Category" class="img-fluid">
+                            </div>
+                            <div class="col-6 my-auto p-0 m-0">
+                                <p>Decks: 5</p>
+                                <p>Cards: 60</p>
+                            </div>
+                        </div>                        
+                    </a>
+                    <hr>
+                    <a href="#" alt="CompTIA Category" class="category-option">
+                        <div class="row">
+                            <div class="col-6 my-auto p-0 m-0">
+                                <img src="images/js-category.png" alt="compTIA Category" class="img-fluid">
+                            </div>
+                            <div class="col-6 my-auto p-0 m-0">
+                                <p>Decks: 5</p>
+                                <p>Cards: 60</p>
+                            </div>
+                        </div>                        
+                    </a>
+                    <hr>
+                    <!-- End EXAMPLES -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include('footer.php');?>
